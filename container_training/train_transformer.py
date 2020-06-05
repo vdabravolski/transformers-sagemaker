5cd2e9d66732b5e90e6e126839204a79a3690caa
EC2 Default User <ec2-user@ip-172-16-6-128.us-east-2.compute.internal>
2020-06-05 19:19:58 +0000

    adding support of custom training files for language modelling task

--- container_training/train_transformer.py
+++ container_training/train_transformer.py
@@ -1,7 +1,6 @@
-import logging 
+import logging
 import json
 import os
-import importlib.util
 import sys
 from argparse import ArgumentParser
 import launch
@@ -13,8 +12,9 @@ logging.basicConfig(handlers=[handler])
 
 
 # Transformer specific lists
-BOOL_FLAGS = ["--fp16", "--do_train", "--do_eval"]
+BOOL_FLAGS = ["--fp16", "--do_train", "--do_eval", "--mlm"]
 GLUE_TASKS = ["CoLA", "SST-2", "MRPC", "STS-B", "QQP", "MNLI", "QNLI", "RTE", "WNLI"]
+
 
 def _arg_flags_to_bool(args):
     """
@@ -25,32 +25,35 @@ def _arg_flags_to_bool(args):
 
     This method converts some common flags.
     """
-    
+
     converted_args = []
-    
-    for i, arg in enumerate(args):
-        if (arg in BOOL_FLAGS):
-            if args[i+1].lower()=="true":
-                converted_args.append(arg)
-            del args[i+1] #remove flag value            
+
+    # break down algorithm arguments into pairs
+    arg_pairs = zip(args[::2], args[1::2])
+
+    for arg, value in arg_pairs:
+        if (arg.strip() in BOOL_FLAGS):
+            if (value.strip().lower() == "true"):
+                converted_args.append(arg.strip())
         else:
-            converted_args.append(arg)
-            
+            converted_args.append(arg.strip())
+            converted_args.append(value.strip())
+
     return converted_args
-    
+
 
 def get_training_world():
 
     """
     Calculates number of devices in Sagemaker distributed cluster
     """
-    
+
     # Get params of Sagemaker distributed cluster from predefined env variables
     num_gpus = int(os.environ["SM_NUM_GPUS"])
     num_cpus = int(os.environ["SM_NUM_CPUS"])
     hosts = json.loads(os.environ["SM_HOSTS"])
     current_host = os.environ["SM_CURRENT_HOST"]
-    
+
     # Define PyTorch training world
     world = {}
     world["number_of_processes"] = num_gpus if num_gpus > 0 else num_cpus
@@ -59,7 +62,7 @@ def get_training_world():
     world["machine_rank"] = hosts.index(current_host)
     world["master_addr"] = hosts[0]
     world["master_port"] = "55555" # port is defined by Sagemaker
-    
+
     return world
 
 
@@ -69,31 +72,47 @@ def task_selector(sm_args, transformer_args):
     augments transformer args with ones specific to Sagemaker training runtime,
     and returns training path and augmented argline.
     """
-    
+
     # convert boolen flags
     transformer_args = _arg_flags_to_bool(transformer_args)
-    
+
     # Language modeling problem setup
     if sm_args.nlp_problem.lower() == "language-modeling":        
-        
+
         task_path = os.path.join(os.environ["SAGEMAKER_SUBMIT_DIRECTORY"], 
                                  "transformers/examples/language-modeling/run_language_modeling.py")
-        
-        if sm_args.dataset.lower() == "wiki":
-            
-            # Augment argline based on dataset
-            transformer_args += ["--train_data_file", os.path.join(os.environ['SM_CHANNEL_TRAIN'], 'wiki.train.raw'),
-                                 "--eval_data_file", os.path.join(os.environ['SM_CHANNEL_TEST'], 'wiki.test.raw'),
-                                 "--output_dir", os.environ['SM_OUTPUT_DATA_DIR']]
+
+        if sm_args.dataset is not None:
+            if sm_args.dataset.lower() == "wiki":
+
+                # Augment argline based on dataset
+                transformer_args += ["--train_data_file", os.path.join(os.environ['SM_CHANNEL_TRAIN'], 'wiki.train.raw'),
+                                     "--eval_data_file", os.path.join(os.environ['SM_CHANNEL_TEST'], 'wiki.test.raw')]
+            else:
+                raise ValueError(f"Dataset {sm_args.dataset} is not supported.")
+
         else:
-            raise ValueError(f"Dataset {sm_args.dataset} is not supported.")
-            
+
+            if ("--do_train" in transformer_args) and ("--train_data_file" in transformer_args):
+                file_value_index = transformer_args.index("--train_data_file") + 1
+                filename = transformer_args[file_value_index]
+                # append train file directory to file path
+                transformer_args[file_value_index] = os.path.join(os.environ['SM_CHANNEL_TRAIN'], filename)
+
+            if ("--do_eval" in transformer_args) and ("--eval_data_file" in transformer_args):
+                file_value_index = transformer_args.index("--eval_data_file") + 1
+                filename = transformer_args[file_value_index]
+                # append train file directory to file path
+                transformer_args[file_value_index] = os.path.join(os.environ['SM_CHANNEL_TEST'], filename)
+
+        transformer_args += ["--output_dir", os.environ['SM_OUTPUT_DATA_DIR']]
+
     # Text classification problem setup        
     elif sm_args.nlp_problem.lower() == "text-classification":
-        
+
         task_path = os.path.join(os.environ["SAGEMAKER_SUBMIT_DIRECTORY"], 
                                  "transformers/examples/text-classification/run_glue.py")
-            
+
         if "--task_name" in transformer_args:
             task_name = transformer_args[transformer_args.index("--task_name")+1]
 
@@ -121,10 +140,10 @@ if __name__ == "__main__":
                                                                                https://github.com/huggingface/transformers/tree/master/examples#the-big-table-of-tasks.")
     parser.add_argument('--dataset', type=str, default=None, help="Define which dataset to use.")
     sm_args, transformer_args = parser.parse_known_args()
-    
+
     # Get task script and its cofiguration
     task_script, transformer_args = task_selector(sm_args, transformer_args)
-    
+
     # Derive parameters of distributed training cluster in Sagemaker
     world = get_training_world()
 
@@ -133,7 +152,7 @@ if __name__ == "__main__":
     launch_config = ["--nnodes", str(world['number_of_machines']), "--node_rank", str(world['machine_rank']),
                      "--nproc_per_node", str(world['number_of_processes']), "--master_addr", world['master_addr'], 
                      "--master_port", world['master_port']]
-        
+
     # Launch distributed training. Note, that launch script configuration is passed as script arguments
-    sys.argv = [""] + launch_config + [task_script]+ transformer_args
-    launch.main()    
+    sys.argv = [""] + launch_config + [task_script] + transformer_args
+    launch.main()
